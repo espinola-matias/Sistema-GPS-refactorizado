ae27b02275a1abfc21f9aa4d296720b77b796ee2
matiasespinola <rojasmati98@gmail.com>
2025-11-23 01:35:13 -0300

    MEtodo mostrar

--- vista.py
+++ vista.py
@@ -8,3 +8,15 @@ class MostrarCamino:
         self.inicio = inicio
         self.destino = destino
         self.dimension_min = dimension_min
+
+    def mostrar_camino(self):
+        ciudad_con_camino = [fila [:] for fila in self.tablero] # Realizo la copia del tablero para poder mostrar el camino si hay 
+
+        if self.camino:
+            for fila, columna in self.camino:
+                if (fila, columna) != self.entrada and (fila, columna) != self.salida:
+                    ciudad_con_camino[fila][columna] = self.ruta
+
+        #Realizo una sobre posicion de entrada y salida en caso de algun error y no marque 
+        ciudad_con_camino[self.entrada[0]][self.entrada[1]] = self.inicio
+        ciudad_con_camino[self.salida[0]][self.salida[1]] = self.destino
