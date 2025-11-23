0d991674e999430353b5be49e7edae1836a77ec5
matiasespinola <rojasmati98@gmail.com>
2025-11-23 01:39:27 -0300

    definimos objetos

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from tablero import Tablero
 from algoritmos import Solucionador
 from vista import MostrarCamino
+
+def ejecutar ():
+    libre = "⬜"
+    ruta = "◾"
+    inicio = "✅"
+    destino = "❌"
+    caracter_edificio = "🏨"
+    caracter_agua = "♒"
+    porcentaje_agua = 4
+    dimension_min = 5
+    caracter_obstaculo = "🚫"
+
+    print("-- Bienvenido a la ciudad --")
+    while True:
+        try:
+            dimension = int(input(f"*Dime el tamaño de la ciudad. OBS('El tamaño debe ser mayor a {dimension_min}!'): "))
+            if dimension <= dimension_min:
+                print(f"El valor debe ser mayor a {dimension_min}, ajustamos a un valor por defecto!")
+                dimension = dimension_min
+            break
+        except ValueError:
+            print("Favor solo ingresar numeros!")
+
+    tablero = Tablero(dimension, caracter_edificio, caracter_agua, porcentaje_agua, libre, inicio, destino)
+    tablero.mostrar()
