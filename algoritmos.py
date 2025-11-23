cace5f75e29dfa0bb9f443b7aa3abb2c4f225179
matiasespinola <rojasmati98@gmail.com>
2025-11-23 01:13:09 -0300

    Movimientos validos

--- algoritmos.py
+++ algoritmos.py
@@ -11,3 +11,12 @@ class ObtenerVecinos:
         # configuracion de costos
         self.costo_normal = 1
         self.costo_agua = 5  # Puede cruzar por el agua pero tiene un costo mayor
+
+    def obtener(self, posicion_actual, permitir_agua):
+        fila, columna = posicion_actual
+        direcciones = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        movimientos_validos = []
+
+        for cambio_fila, cambio_columna in direcciones:
+            nueva_fila = fila + cambio_fila
+            nueva_columna = columna + cambio_columna
