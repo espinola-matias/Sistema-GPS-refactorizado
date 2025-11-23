090a84193f85a2ff138c29799b139815fe6710f6
matiasespinola <rojasmati98@gmail.com>
2025-11-23 01:34:22 -0300

    instanciamos

---
+++ vista.py
@@ -0,0 +1,10 @@
+class MostrarCamino:
+    def __init__(self, tablero, camino, entrada, salida, ruta, inicio, destino, dimension_min):
+        self.tablero = tablero
+        self.camino = camino
+        self.entrada = entrada
+        self.salida = salida
+        self.ruta = ruta
+        self.inicio = inicio
+        self.destino = destino
+        self.dimension_min = dimension_min
